34b92e57ba4685c7a569ecdc27b8dca4bb788537
loveyunye <13755867654@163.com>
2020-12-06 23:15:16 +0800

    no message

--- app/routes/user.js
+++ app/routes/user.js
@@ -5,12 +5,15 @@ const Auth = require('../middleWare/auth');
 
 router.get('/', Auth.admin, UserCtl.list);
 router.get('/all', Auth.admin, UserCtl.all);
-router.post('/', UserCtl.create);
+
+router.post('/', Auth.admin, UserCtl.create);
 router.patch('/:id', Auth.admin, UserCtl.edit);
+
 router.delete('/:id', Auth.admin, UserCtl.delete);
 router.get('/detail/:id', Auth.admin, UserCtl.getWorks);
 
-router.post('/mobile', UserCtl.editMobile);
 router.get('/mobile', Auth.mobile, UserCtl.getMy);
+router.post('/mobile', Auth.mobile, UserCtl.create);
+router.post('/mobile/:id', UserCtl.edit);
 
 module.exports = router;
